10aee7db78f3a1d78ecf59f10b3789ef054e3c77
khaldoun36 <khaldoun36@gmail.com>
2022-11-02 19:36:11 +0400

    second commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryClr: "#FF6B00",
+        secondClr: "#BADEFF",
+        thirdClr: "#FFEBB2",
+        neutralClr: "#dee2e6",
+        textDark: "#202020",
+        textLight: "#333533",
+      },
+      screens: {
+        md: "821px",
+      },
+    },
+  },
+  plugins: [],
+};
